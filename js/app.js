c16db4bc1312016e09f21624bbb9d818f894f0f7
Samuel Robertson <samuel.robertson93@gmail.com>
2016-01-01 17:17:57 -0500

    added social media links and html to timeline

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let app = angular.module('myApp',['ui.router', 'mm.foundation','typer']);
+let app = angular.module('myApp',['ui.router', 'mm.foundation','typer','angular-timeline','angular-scroll-animate','ngSanitize']);
 
 // Scroll Animations
 app.value('duScrollDuration', 2000).value('duScrollOffset', 30)
@@ -29,14 +29,69 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 
 });
 
-app.controller('MainCtrl', function($scope,$location, $anchorScroll){
+app.controller('MainCtrl', function($scope, $document, $timeout,$location, $anchorScroll, $sce){
 
   // Navigation Control
   $scope.gotoAnchor = function(anchor,$event) {
-    console.log($event);
+
       $location.hash(anchor);
       $anchorScroll();
   };
+
+
+
+
+  $scope.events = [{
+    badgeClass: 'info',
+    title: 'Coding House',
+    when:' October 2015 - January 2016',
+    content: 'A Silicon Valley Coding BootCamp',
+    contentHtml:'Learn more here: <a href="https://codinghouse.co/">Coding House</a>'
+  },
+  {
+    badgeClass: 'info',
+    title: 'The Pennsylvania State University',
+    when:' September 2011 - August 2015',
+    content:'Bachelor of Science (B.S) in Mechanical Engineering',
+    contentHtml:'Learn more here: <a href="http://www.psu.edu/">The Pennsylvania State University</a>'
+  },
+  {
+    badgeClass: 'info',
+    title: 'Second heading',
+    when:'somtime',
+    content: 'More awesome content.'
+  },
+  {
+    badgeClass: 'info',
+    title: 'Second heading',
+    when:'somtime',
+    content: 'More awesome content.'
+  }
+];
+
+$scope.animateElementIn = function($el) {
+		$el.removeClass('timeline-hidden');
+		$el.addClass('bounce-in');
+	};
+
+	// optional: not mandatory (uses angular-scroll-animate)
+	$scope.animateElementOut = function($el) {
+		$el.addClass('timeline-hidden');
+		$el.removeClass('bounce-in');
+	};
+
+	$scope.leftAlign = function() {
+		$scope.side = 'left';
+	}
+
+	$scope.rightAlign = function() {
+		$scope.side = 'right';
+	}
+
+	$scope.defaultAlign = function() {
+		$scope.side = ''; // or 'alternate'
+	}
+
 
 
 });
